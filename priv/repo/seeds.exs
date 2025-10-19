6bb8c6c6eb0e7b6f9ca9943f46762f3ff005c5fc
Damaris <damarismjuares@gmail.com>
2025-10-19 17:23:56 -0300

    a

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -7,15 +7,9 @@ Repo.delete_all(Users)
 # --- Usuarios ---
 sofia = Repo.insert!(%Users{username: "Sofía",birth_date: ~D[2000-01-01]})
 mateo = Repo.insert!(%Users{username: "Mateo", birth_date: ~D[2003-01-01]})
-gustavo = Repo.insert!(%Users{username: "Gustavo", birth_date: ~D[1955-07-01]})
-lucia = Repo.insert!(%Users{username: "Lucia",birth_date: ~D[1999-01-01]})
 
 # --- Monedas ---
 btc = Repo.insert!(%Money{name: "BTC", price: 55000.0})
-eth = Repo.insert!(%Money{name: "ETH", price: 3000.0})
-ars = Repo.insert!(%Money{name: "ARS", price: 0.0012})
-usd = Repo.insert!(%Money{name: "USD", price: 1.0})
-eur = Repo.insert!(%Money{name: "EUR", price: 1.18})
 
 # --- Transacciones ---
 
